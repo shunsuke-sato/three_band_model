f39e327cf7022cc4afecfbe6e81e551b615428fb
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-01-21 11:45:59 +0100

    Modified momentum matrix to avoid divergence.

--- src/dt_evolve.f90
+++ src/dt_evolve.f90
@@ -28,13 +28,9 @@ subroutine dt_evolve(it) ! Now coding
     eps_t(1) = eps_d
     eps_t(2) = -0.5d0/mass_v*(kr(ikr)**2+kz(ikz)**2)
     eps_t(3) = eps_g +0.5d0/mass_c*(kr(ikr)**2+kz(ikz)**2)
-    de12 = eps_t(2) - eps_t(1)  
+    de12 = eps_t(1) - eps_t(2)  
     zHmat(1,1) = eps(1,ik); zHmat(2,2) = eps(2,ik); zHmat(3,3) = eps(3,ik)
-    if(abs(de12)*Ry*2d0>1d0)then
-      zHmat(1,2) = -zI*piz_dv*Et_old/(eps_t(1)-eps_t(2)); zHmat(2,1)=conjg(zHmat(1,2))
-    else
-      zHmat(1,2) = 0d0; zHmat(2,1) = 0d0
-    end if
+    zHmat(1,2) = -zI*piz_dv*Et_old*de12/(de12**2+deps12_2); zHmat(2,1)=conjg(zHmat(1,2))
     zHmat(1,3) = -zI*piz_dc*Et_old/(eps_t(1)-eps_t(3)); zHmat(3,1)=conjg(zHmat(1,3))
 
     zHmat(2,3) = -zI*piz_vc*Et_old/(eps_t(2)-eps_t(3)); zHmat(3,2)=conjg(zHmat(2,3))
@@ -57,14 +53,10 @@ subroutine dt_evolve(it) ! Now coding
     eps_t(1) = eps_d
     eps_t(2) = -0.5d0/mass_v*(kr(ikr)**2+kz(ikz)**2)
     eps_t(3) = eps_g +0.5d0/mass_c*(kr(ikr)**2+kz(ikz)**2)
-    de12 = eps_t(2) - eps_t(1)  
+    de12 = eps_t(1) - eps_t(2)  
 
     zHmat(1,1) = eps(1,ik); zHmat(2,2) = eps(2,ik); zHmat(3,3) = eps(3,ik)
-    if(abs(de12)*Ry*2d0>1d0)then
-      zHmat(1,2) = -zI*piz_dv*Et_new/(eps_t(1)-eps_t(2)); zHmat(2,1)=conjg(zHmat(1,2))
-    else
-      zHmat(1,2) = 0d0; zHmat(2,1) = 0d0
-    end if
+    zHmat(1,2) = -zI*piz_dv*Et_new*de12/(de12**2+deps12_2); zHmat(2,1)=conjg(zHmat(1,2))
     zHmat(1,3) = -zI*piz_dc*Et_new/(eps_t(1)-eps_t(3)); zHmat(3,1)=conjg(zHmat(1,3))
     zHmat(2,3) = -zI*piz_vc*Et_new/(eps_t(2)-eps_t(3)); zHmat(3,2)=conjg(zHmat(2,3))
 
